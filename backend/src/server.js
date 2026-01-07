874dbacab9668bfb4cf3672be3fdd94d74f8c78b
Wyz1710 <scutter171006@gmail.com>
2026-01-07 08:29:26 +0700

    feat: database config and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import path from "path";
+import { clerkMiddleware } from '@clerk/express'
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
 const __dirname = path.resolve()
+
+app.use(clerkMiddleware()) //add auth object under req => req.auth
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({message:'Success'});
@@ -19,4 +23,7 @@ if(ENV.NODE_ENV === "production") {
     });
 }
 
-app.listen(ENV.PORT, () => console.log("Sever is starting and running"));
+app.listen(ENV.PORT, () => {
+    console.log("Sever is starting and running")
+    connectDB();
+});
