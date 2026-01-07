7abb75d256d2299578dd08cbe9d445075fc8237b
Wyz1710 <scutter171006@gmail.com>
2026-01-07 09:48:12 +0700

    feat: authentication implement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import path from "path";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js"
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { start } from "repl";
 
 const app = express();
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-app.use(clerkMiddleware()) //add auth object under req => req.auth
+app.use(express.json());
+app.use(clerkMiddleware()); //add auth object under req => req.auth
+
+app.use("/api/inngest", serve({client:inngest, functions}));
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({message:'Success'});
@@ -17,7 +22,7 @@ app.get("/api/health", (req, res) => {
 
 //deployment
 if(ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../admin/dist")))
+    app.use(express.static(path.join(__dirname, "../admin/dist")));
 
     app.get("/{*any}", (req,res) => {
         res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
