3f098033f8e1b209244b98f3f46fd5e10124cbe6
Wyz1710 <scutter171006@gmail.com>
2026-01-09 20:37:17 +0700

    feat: added user routing

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,6 +23,8 @@ app.get("/api/health", (req, res) => {
     res.status(200).json({message:'Success'});
 });
 
+
+
 //deployment
 if(ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../admin/dist")));
